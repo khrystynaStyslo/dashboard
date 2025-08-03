0e82395882fb17fb74acbccca03c59b03f0a602f
khhhrystyyna <khrystyna.styslo@gmail.com>
2025-08-04 00:15:43 +0300

    fixed pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { DashboardProvider } from '@/app/providers'
-import { DashboardPage } from '@/pages/DashboardPage'
+import { DashboardPage } from '@/pages/dashboard/page'
 
 function App() {
   return (
--- src/pages/dashboard/page.tsx
+++ src/pages/dashboard/page.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import { Dashboard } from '@/widgets/dashboard'
 
 export function DashboardPage() {
-  return <Dashboard />
+    return <Dashboard />
 }
